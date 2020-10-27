f23750a97cf76fadf0df75db07f3d43522920ddb
Harsh Goyal <hgoyal015@gmail.com>
2020-10-27 15:31:22 +0530

    Made some changes in SVM

--- svm.py
+++ svm.py
@@ -1,3 +1,5 @@
+from lightgbm import LGBMClassifier
+from sklearn.ensemble import RandomForestClassifier
 import re
 import nltk
 from nltk.stem import *
@@ -40,57 +42,102 @@ def cor_selector(X, y, num_feats):
 
 data = pd.read_csv('out.csv')
 df = data
-df.drop(columns=['Label'])
+df = df.drop(columns=['Label'])
 X = df
+print(np.shape(X))
 y = data['Label']
 print(np.shape(X))
-num_feats = 600
+
+
+num_feats = 750
+feature_name = X.columns.tolist()
 cor_support, cor_feature = cor_selector(X, y, num_feats)
-# print(cor_support)
-# print(cor_feature)
 
-print(str(len(cor_feature)), 'selected features')
-# my_data = genfromtxt('out.csv', delimiter=',')
+our_feature = []
 
-my_data = pd.read_csv('out.csv')
-
-df = my_data[cor_feature]
-df.drop(columns=['Label'])
-
-X_ori = np.array(df)
-print(np.shape(X_ori))
-Y_ori = np.array(my_data)
-print(type(my_data))
-# print(my_data)
-
-data_length = 2479
-
-train_len = (int)(data_length*(0.7))
-test_len = (data_length-train_len)
-
-# print(my_data)
-
-# y_train=my_data[:,-1]
-y_train = Y_ori[1:train_len, -1]
-
-y_test = Y_ori[train_len:, -1]
-# print(ans)
-
-# print(ans[0])
-
-X_train = X_ori[1:train_len, :-1]
-# X_train=X_ori[:,:-1]
-
-X_test = X_ori[train_len:, :-1]
-
-# print(X_ori)
-
-eeg_svc = SVC(C=1.0, kernel="linear")
-# print(type(eeg_svc))
-
-eeg_svc.fit(X_train, y_train.ravel())
-print("Training Accuracy:", (eeg_svc.score(X_train, y_train.ravel()))*100, "%")
+our_feature.append(["Pearson Correlation", cor_feature])
 
 
-eeg_svc.predict(X_test)
-print("Test Accuracy:", (eeg_svc.score(X_test, y_test.ravel()))*100, "%")
+# Chi-Squared
+X_norm = MinMaxScaler().fit_transform(X)
+chi_selector = SelectKBest(chi2, k=num_feats)
+chi_selector.fit(X_norm, y)
+chi_support = chi_selector.get_support()
+chi_feature = X.loc[:, chi_support].columns.tolist()
+
+
+our_feature.append(["Chi-Squared", chi_feature])
+
+# Recursive Feature Elimination
+rfe_selector = RFE(estimator=LogisticRegression(),
+                   n_features_to_select=num_feats, step=30, verbose=5)
+rfe_selector.fit(X_norm, y)
+rfe_support = rfe_selector.get_support()
+rfe_feature = X.loc[:, rfe_support].columns.tolist()
+print(str(len(rfe_feature)), 'selected features')
+
+
+our_feature.append(["Recursive Feature Elimination", rfe_feature])
+
+
+# Random Forest
+
+embeded_rf_selector = SelectFromModel(
+    RandomForestClassifier(n_estimators=100), max_features=num_feats)
+embeded_rf_selector.fit(X, y)
+
+embeded_rf_support = embeded_rf_selector.get_support()
+embeded_rf_feature = X.loc[:, embeded_rf_support].columns.tolist()
+print(str(len(embeded_rf_feature)), 'selected features')
+
+our_feature.append(["Random Forest", embeded_rf_feature])
+
+# LightGBM
+
+
+lgbc = LGBMClassifier(n_estimators=500, learning_rate=0.05, num_leaves=32, colsample_bytree=0.2,
+                      reg_alpha=3, reg_lambda=1, min_split_gain=0.01, min_child_weight=40)
+
+embeded_lgb_selector = SelectFromModel(lgbc, max_features=num_feats)
+embeded_lgb_selector.fit(X, y)
+
+embeded_lgb_support = embeded_lgb_selector.get_support()
+embeded_lgb_feature = X.loc[:, embeded_lgb_support].columns.tolist()
+print(str(len(embeded_lgb_feature)), 'selected features')
+
+our_feature.append(["LightGBM", embeded_lgb_feature])
+
+
+for i in our_feature:
+    my_data = pd.read_csv('out.csv')
+    print("---------------------------------------",
+          i[0], "------------------------------")
+    # print(my_data.head)
+    # print(i[1])
+    df = my_data[i[1]]
+    # df = my_data
+    X_ori = np.array(df)
+    print(np.shape(X_ori))
+    Y_ori = np.array(my_data['Label'])
+    # print(type(my_data))
+
+    data_length = np.shape(X_ori)[0]
+    print(data_length)
+    data_length = 1000
+    train_len = (int)(data_length*(0.7))
+    test_len = (data_length-train_len)
+    y_train = Y_ori[1:train_len]
+    y_test = Y_ori[train_len:]
+    X_train = X_ori[1:train_len, :-1]
+    X_test = X_ori[train_len:, :-1]
+
+    # print(X_ori)
+
+    eeg_svc = SVC(C=1.0, kernel="linear")
+    # print(type(eeg_svc))
+
+    eeg_svc.fit(X_train, y_train.ravel())
+    print("Training Accuracy:", (eeg_svc.score(X_train, y_train.ravel()))*100, "%")
+
+    eeg_svc.predict(X_test)
+    print("Test Accuracy:", (eeg_svc.score(X_test, y_test.ravel()))*100, "%")
